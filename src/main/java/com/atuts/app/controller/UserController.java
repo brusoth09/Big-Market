399a4b7e7fe3874be2016f95608ddcdff628e892
brusoth09 <brusoth09@gmail.com>
2016-04-22 15:05:09 +0530

    Controller method signature added.

--- src/main/java/com/atuts/app/controller/UserController.java
+++ src/main/java/com/atuts/app/controller/UserController.java
@@ -6,11 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Created by BurusothmanA on 3/14/2016 6:20 PM.
+ * Controller class to handle requests related user login
+ *
  */
 @Controller
 public class UserController {
 
+    /**
+     * Method to get handle get get request to login page.
+     *
+     * @param model interface that defines a holder for model attributes. encapsulates the application data. pre-populated model.
+     * @return the view name need to be render.
+     */
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage(Model model){
         model.addAttribute("key","value");
